1a2d919056be4b0a2eba2de8d2b17ed06e7b4867
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-09-30 12:41:09 +0530

    commit inheritence and abstraction

--- src/basicsofjava/demo.java
+++ src/basicsofjava/demo.java
@@ -1,8 +1,23 @@
 package basicsofjava;
 
+/*
+* Access Modifier -> public, private, protected, default
+* Non-Access Modifier -> Abstract, static, final */
+
+
 public class demo {
     public static void main(String[] args) {
         demoDetails d = new demoDetails("Sidh",20);
         System.out.println(d);
+
+        overloadingDemo sh = new overloadingDemo();
+        System.out.println("Area Of Square: "+sh.area(10));
+        System.out.println("Area Of Rectangle: "+sh.area(10,20));
+        System.out.println("Area Of Triangle: "+sh.area(10,20,30));
+        System.out.println("-------------------------");
+        inheritenceDemo s = new inheritenceDemo();
+
+        cat c = new cat();
+        c.sound();
     }
 }

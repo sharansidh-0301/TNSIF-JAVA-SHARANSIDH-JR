48f7da7231faf848a9bb8145404ef2fa25dc0046
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-10-09 15:03:32 +0530

    commit "Throw Demo"

--- src/day10/ExceptionHandlingDemo.java
+++ src/day10/ExceptionHandlingDemo.java
@@ -11,15 +11,16 @@ Error - are usually beyond the control of the programmer. and we should not try 
 public class ExceptionHandlingDemo {
     public static void main(String[] args) {
         System.out.println("Main Method Performing.......");
-        int result =0;
-        try{
-            result =  demo1.perform(12,0);
-        }catch(Exception e){
-            System.out.println("You got error : "+e.getMessage());
-            System.out.println("Please Change the Input...");
-        }finally {
-            System.out.println("Finally Blocks Running.......");
-        }
+//        int result =0;
+//        try{
+//            result =  demo1.perform(12,0);
+//        }catch(Exception e){
+//            System.out.println("You got error : "+e.getMessage());
+//            System.out.println("Please Change the Input...");
+//        }finally {
+//            System.out.println("Finally Blocks Running.......");
+//        }
+//
 
     }
 }
--- src/day10/throwDemo.java
+++ src/day10/throwDemo.java
@@ -6,4 +6,13 @@ package day10;
 
 public class throwDemo {
 
+    public static void main(String[] args) {
+        int num=1;
+        int den=0;
+
+        if (den==0) {
+        throw new ArithmeticException("Cannot divide by zero");
+        }
+        System.out.print(num/den);
+    }
 }

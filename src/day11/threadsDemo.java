8d3b9967fab043775098784aeeefc7f3c3883faf
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-10-09 15:55:04 +0530

    Updated threadsDemo.java

---
+++ src/day11/threadsDemo.java
@@ -0,0 +1,38 @@
+package day11;
+
+
+/* Thread is a Class, which comes from lang package.
+*  A process is program that executes as a single thread
+*
+*
+*  Multithreading in java - the process of executing multiple threads simultaneously
+*
+* */
+
+//Implementing thread using class
+class MyTheard extends Thread{
+    public void run(){
+        System.out.println("Thread started Running.......");
+    }
+}
+
+//Implementing Thread using Runnable Interface
+class myRunnable implements Runnable{
+    public void run(){
+        System.out.println("Thread started Running from interface.......");
+    }
+}
+
+
+public class threadsDemo{
+    public static void main(String[] args) {
+
+        MyTheard mt = new MyTheard();
+        mt.start();
+
+        myRunnable r = new myRunnable();
+        Thread th = new Thread(r);
+        th.start();
+    }
+
+}

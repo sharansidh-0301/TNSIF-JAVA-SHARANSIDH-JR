0c9ffec27be88b24f5c36d8d132279290b85518d
SHARANSIDH J R <sharansidh0301@gmail.com>
2025-11-25 21:47:13 +0530

    Delete T030307508_SHARANSIDH_J_R_ShopOwner_Angular/ShopOwner directory

--- T030307508_SHARANSIDH_J_R_ShopOwner_Angular/ShopOwner/src/main.ts
+++
@@ -1,7 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { ShopOwnerModule } from './app/ShopOwner.module';
-
-platformBrowserDynamic()
-  .bootstrapModule(ShopOwnerModule)
-  .catch((err) => console.error(err));
